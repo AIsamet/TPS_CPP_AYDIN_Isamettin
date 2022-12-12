c758ecc294f72d58177e0e5b2fc4e3592c7749d0
AIsamet <isamet2067@gmail.com>
2022-12-12 23:08:19 +0100

    Created new empty classes; updated TODO list

---
+++ TP refactor/Checks.h
@@ -0,0 +1,16 @@
+#ifndef Checks_h
+#define Checks_h
+
+#include "Game.h"
+
+// Classe fonctionnelle gerant toutes les sorties
+class Checks
+{
+public:
+
+
+private:
+
+};
+
+#endif
---
+++ TP refactor/Outputs.h
@@ -0,0 +1,16 @@
+#ifndef Outputs_h
+#define Outputs_h
+
+#include "Game.h"
+
+// Classe fonctionnelle gerant toutes les sorties
+class Outputs
+{
+public:
+
+	
+private:
+
+};
+
+#endif
